4a8933127b10412bf70358f654915000576440fe
jrkagan2 <jordan.kagan@vitruvian-labs.com>
2025-01-16 15:49:39 -0500

    fixed font build issue

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,5 @@
 import { Inter,Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
-export const lusi = Lusitana({ weight: "400"});
+export const lusitana = Lusitana({ weight: "400", subsets: ['latin']});
 
